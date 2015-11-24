75154510c9b6704c74cd398d1a64827e1ea92015
Manoj Gahlawat <manoj.gahlawat@gmail.com>
2015-11-24 19:59:34 +0100

    Simple JxavaScript logic based game
    Simple Tic Tac Toe game.

---
+++ main.js
@@ -0,0 +1,83 @@
+
+		//initiation 
+
+		function startGame() {
+		// checking with a loop and count +1 for every filled box
+			for (var i = 1; i <= 9; i = i + 1) {
+				clearBox(i);
+			}
+		//taking random reference to X to switch turns.
+			document.turn = "X";
+			if (Math.random() < 0.5) {
+				document.turn = "O";
+			}
+			document.winner = null;
+			setMessage(document.turn + " gets to start.");
+		}
+		
+		function setMessage(msg) {
+			document.getElementById("message").innerText = msg;
+		}
+
+		
+		//making sure that X and O both switch turns.
+
+
+		function nextMove(square) {
+			if (document.winner != null) {
+				setMessage(document.winner + " already won the game.");
+			} else if (square.innerText == "") {
+				square.innerText = document.turn;
+				switchTurn();
+			} else {
+				setMessage("That square is already used.");
+			}
+		}
+		// Auto switch turns 
+		function switchTurn() {
+		
+			if (checkForWinner(document.turn)) {
+				setMessage("Congratulations, " + document.turn + "!  You win!");
+				document.winner = document.turn;
+			} else if (document.turn == "X") {
+				document.turn = "O";
+				setMessage("It's " + document.turn + "'s turn!");
+			} else {
+				document.turn = "X";
+				setMessage("It's " + document.turn + "'s turn!");
+			}
+		}
+		// array assosiated with the squares to check winning combination for either X or O
+
+		function checkForWinner(move) {
+			var result = false;
+			if (checkRow(1, 2, 3, move) || 
+				checkRow(4, 5, 6, move) ||
+				checkRow(7, 8, 9, move) || 
+				checkRow(1, 4, 7, move) ||
+				checkRow(2, 5, 8, move) ||
+				checkRow(3, 6, 9, move) ||
+				checkRow(1, 5, 9, move) ||
+				checkRow(3, 5, 7, move)) {
+				
+				result = true;
+			}
+			return result;
+		}
+		// winning verification
+
+		function checkRow(a, b, c, move) {
+			var result = false;
+			if (getBox(a) == move && getBox(b) == move && getBox(c) == move) {
+				result = true;
+			}
+			return result;
+		}
+		
+		function getBox(number) {
+			return document.getElementById("s" + number).innerText;
+		}
+		
+		function clearBox(number) {
+			document.getElementById("s" + number).innerText = "";
+		}
